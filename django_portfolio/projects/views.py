77f7b05ca2db79c739896dd941da37fa26f84ac6
Aronsen <tord.aronsen@skatteetaten.no>
2021-03-24 14:31:23 +0100

    Make use of templates

--- django_portfolio/projects/views.py
+++ django_portfolio/projects/views.py
@@ -1,7 +1,6 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def project_list(request):
-    return HttpResponse("<h1>Batman is bat and man<h1>")
+    return render(request, 'projects/index.html')
